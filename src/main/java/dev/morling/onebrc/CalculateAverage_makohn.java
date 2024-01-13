60d2174fcc9f825046297d8ffd16de2a0cec346a
Marek Kohn <me@makohn.de>
2024-01-13 20:17:01 +0100

    Use concurrency

--- src/main/java/dev/morling/onebrc/CalculateAverage_makohn.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_makohn.java
@@ -15,11 +15,12 @@
  */
 package dev.morling.onebrc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CalculateAverage_makohn {
 
@@ -47,28 +48,65 @@ public class CalculateAverage_makohn {
         }
     }
 
-    private static final Map<String, Measurement> res = new TreeMap<>();
+    private static final int CHUNK_SIZE = 1_000_000;
 
-    public static void main(String[] args) throws IOException {
-        final var in = new BufferedReader(new FileReader(FILE));
+    private record ChunkProcessor(List<String> chunk, int chunkNr) implements Callable<Map<String, Measurement>> {
 
-        String line;
-        while ((line = in.readLine()) != null) {
-            final var l = line;
-            final var kv = l.split(";");
-            final var k = kv[0];
-            final var v = Double.parseDouble(kv[1]);
-            if (res.containsKey(k)) {
-                final var m = res.get(k);
-                m.min = Math.min(m.min, v);
-                m.max = Math.max(m.max, v);
-                m.sum += v;
-                m.count++;
-            } else {
-                res.put(k, new Measurement(v));
+        @Override
+        public Map<String, Measurement> call() {
+            final var map = new HashMap<String, Measurement>(CHUNK_SIZE);
+            for (final var line : chunk) {
+                final var kv = line.split(";");
+                final var key = kv[0];
+                final var value = Double.parseDouble(kv[1]);
+                if (map.containsKey(key)) {
+                    final var current = map.get(key);
+                    current.min = Math.min(current.min, value);
+                    current.max = Math.max(current.max, value);
+                    current.count++;
+                    current.sum += value;
+                }
+                else {
+                    map.put(key, new Measurement(value));
+                }
             }
+            return map;
         }
 
-        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (final var in = Files.lines(Paths.get(FILE));
+             final var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())) {
+            final var spliterator = in.spliterator();
+            final var futures = new ArrayList<Future<Map<String, Measurement>>>();
+            int nr = 0;
+            while (true) {
+                final var chunk = new ArrayList<String>(CHUNK_SIZE);
+                int i = 0;
+                while (i < CHUNK_SIZE && spliterator.tryAdvance(chunk::add))
+                    i++;
+                if (chunk.isEmpty())
+                    break;
+                futures.add(executor.submit(new ChunkProcessor(chunk, nr)));
+                nr++;
+            }
+            final var res = new TreeMap<>(futures.removeFirst().get());
+            for (final var future : futures) {
+                final var tmp = future.get();
+                for (final var entry : tmp.entrySet()) {
+                    final var key = entry.getKey();
+                    final var value = entry.getValue();
+                    if (res.containsKey(key)) {
+                        final var cur = res.get(key);
+                        cur.min = Math.min(cur.min, value.min);
+                        cur.max = Math.max(cur.max, value.max);
+                        cur.count += value.count;
+                        cur.sum += value.sum;
+                    }
+                }
+            }
+            System.out.println(res);
+        }
     }
 }
